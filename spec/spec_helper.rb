d00c94c9fed5d96449f6a981808f9e0e85c88ba7
Markus Schirp <mbj@seonic.net>
2013-02-25 00:39:06 +0100

    Use Devtools.init_spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,8 @@
 $: << 'lib'
 
 require 'sitemap_xml'
-
-# require spec support files and shared behavior
-Dir[File.expand_path('../{support,shared}/**/*.rb', __FILE__)].each { |f| require f }
-
-if RUBY_VERSION < '1.9.0'
-  require 'rspec/autorun'
-end
+require 'devtools'
+Devtools.init_spec_helper
 
 RSpec.configure do |config|
   config.extend(LetMockHelper)
